3148c4157adfe3a93d092a5251e8d361d5c11dd7
Till Bey <till.bey@surf.nl>
2026-02-18 11:20:50 +0100

    pin OpenAPC download to specific version

--- sources/openapc/download.sh
+++ sources/openapc/download.sh
@@ -1,6 +1,11 @@
 source ./setup.sh openapc
 
-files="https://github.com/OpenAPC/openapc-de/raw/refs/heads/master/data/apc_de.csv https://github.com/OpenAPC/openapc-de/raw/refs/heads/master/data/apc_de_additional_costs.csv https://github.com/OpenAPC/openapc-de/raw/refs/heads/master/data/bpc.csv https://github.com/OpenAPC/openapc-de/raw/refs/heads/master/data/transformative_agreements/transformative_agreements.csv"
+files="\
+https://github.com/OpenAPC/openapc-de/raw/refs/tags/v4.167.1-0-1/data/apc_de.csv \
+https://github.com/OpenAPC/openapc-de/raw/refs/tags/v4.167.1-0-1/data/apc_de_additional_costs.csv \
+https://github.com/OpenAPC/openapc-de/raw/refs/tags/v4.167.1-0-1/data/bpc.csv \
+https://github.com/OpenAPC/openapc-de/raw/refs/tags/v4.167.1-0-1/data/transformative_agreements/transformative_agreements.csv\
+"
 
 for file in $(echo $files)
     do echo $file >> $log_file
