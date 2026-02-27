dd2ef75be7c5d985bc73b35fabfb4d8b5a442a39
Till Bey <till.bey@surf.nl>
2026-02-27 14:54:26 +0100

    factor out helper function

--- setup.sh
+++ setup.sh
@@ -1,9 +1,14 @@
-raw_data_path=$(jq -r '."raw-data-path"' <config.json)/${1}/
-log_path=$(pwd)/$(jq -r '."log-path"' <config.json)/${1}/
-log_file=${log_path}download-$(date -Iseconds -u).txt
+raw_data_path=$(jq -r '."raw-data-path"' <config.json)/${2}/
+log_path=$(pwd)/$(jq -r '."log-path"' <config.json)/${2}/
+log_file=${log_path}${1}-$(date -Iseconds -u).txt
 
 mkdir -p $log_path
 touch $log_file
 
 mkdir -p $raw_data_path
 cd $raw_data_path
+
+function zenodo_filelist {
+    curl https://zenodo.org/api/deposit/depositions/${1}/files | jq -r '.[].links.download'
+}
+
