be18031ddf148f1cd4471bfe09a98d8e27c3a674
Till Bey <till.bey@surf.nl>
2026-02-27 14:56:00 +0100

    fix quotes

--- queries/init-ducklake.sql
+++ queries/init-ducklake.sql
@@ -1,5 +1,5 @@
 .shell rm -f {catalogpath}/{datalake}.ducklake
 .shell mkdir -p {catalogpath}
 
-ATTACH 'ducklake:{catalogpath}/{datalake}.ducklake' as {datalake} (DATA_PATH "{datapath}");
+ATTACH 'ducklake:{catalogpath}/{datalake}.ducklake' as {datalake} (DATA_PATH '{datapath}');
 USE {datalake};
