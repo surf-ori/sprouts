2b977ed0e332b0500529b462cb1bc73db8055521
Till Bey <till.bey@surf.nl>
2026-03-03 16:50:23 +0100

    change download path

--- sources/openalex/download.sh
+++ sources/openalex/download.sh
@@ -1,3 +1,3 @@
 source ./setup.sh download openalex
 
->> $log_file aws s3 cp s3://openalex/data /Users/bey00001/dev/sprouts/build/raw-data/openalex --recursive --no-sign-request --region us-east-1
+&>> $logfile aws s3 cp s3://openalex/data . --recursive --no-sign-request --region us-east-1
