2a03efc997fe4a8ff338a1f515479cab17cacdf0
mosart <maurice.vanderfeesten@gmail.com>
2026-04-21 09:48:26 +0200

    update dependencies and improve function definitions in overview.py

--- notebooks/overview.py
+++ notebooks/overview.py
@@ -2,7 +2,7 @@
 # requires-python = ">=3.13"
 # dependencies = [
 #     "altair==6.0.0",
-#     "duckdb==1.5.1",
+#     "duckdb==1.5.2",
 #     "marimo>=0.20.2",
 #     "numpy==2.4.3",
 #     "polars[pyarrow]==1.39.3",
@@ -14,12 +14,12 @@
 
 import marimo
 
-__generated_with = "0.20.4"
+__generated_with = "0.23.2"
 app = marimo.App(width="medium")
 
 
-@app.cell
-async def _():
+@app.cell(hide_code=True)
+async def system():
     import marimo as mo
     import micropip
     # Install the packages when running in WAS
@@ -36,7 +36,7 @@ async def _():
 
 
 @app.cell(hide_code=True)
-def _(mo):
+def header(mo):
     mo.md("""
     <div style="
         display: flex;
@@ -65,15 +65,16 @@ def _(mo):
 
 
 @app.cell(hide_code=True)
-def _(mo):
+def introduction(mo):
     mo.md(r"""
-    This dashboard is part of the [**PID to Portal project**](https://communities.surf.nl/en/open-research-information/article/from-pid-to-portal-strengthening-the-open-research-information) from SURF and UNL. Our goals is to create an overview of available and actively queryable Open Research Information Resources / Datasets, that the ORI community can start using freely. [Code available.](https://github.com/surf-ori/sprouts/)
+    This dashboard is part of the [**PID to Portal project**](https://communities.surf.nl/en/open-research-information/article/from-pid-to-portal-strengthening-the-open-research-information) from SURF and UNL. Our goals is to create an overview of available and actively queryable Open Research Information Resources / Datasets, that the ORI community can start using freely. [Code available here.](https://github.com/surf-ori/sprouts/)
+    [![Open in molab](https://marimo.io/molab-shield.svg)](https://molab.marimo.io/github/surf-ori/sprouts/blob/notebooks/notebooks/overview.py)
     """)
     return
 
 
-@app.cell
-def _(mo):
+@app.cell(hide_code=True)
+def background(mo):
     background = mo.md("""
     In our approach, we aim to avoid BigTech. We curate the queryable databases ourselves but welcome others to share their data catalogs. We utilize the DuckLake catalog from DuckDB and store the actual data as Parquet files on an S3-compatible object store. This separation of storage and compute helps us keep costs low.
 
@@ -81,55 +82,28 @@ def _(mo):
 
     This alows you to 'time-travel' in the data, and not only use the last state. For example detecting when an article flips from open access to closed access.
 
-    **Compute:** You can query the datasets directly from your browser (no login required)! When you query the databases, a portion of the requested data is transferred over HTTPS to your local machine where the SQL operations are performed. Larger data requests result in longer transfer times, and the speed of your local machine affects the query completion time. The size of your machine is up to you.
+    **Computev:** You can query the datasets directly from your browser (no login required)! When you query the databases, a portion of the requested data is transferred over HTTPS to your local machine where the SQL operations are performed. Larger data requests result in longer transfer times, and the speed of your local machine affects the query completion time. The size of your machine is up to you.
 
-    At SURF, we also provide ready-made [services](https://www.surf.nl/en/services) for SQL computation, such as a Marimo notebook in a virtual machine on SURF Research Cloud or a Superset dashboard on a Kubernetes cluster.
+    **Existing Components** At SURF, we also provide ready-made [services](https://www.surf.nl/en/services) for SQL computation, such as a Marimo notebook in a virtual machine on SURF Research Cloud or a Superset dashboard on a Kubernetes cluster.
 
-    Below, you will see the ORI data resources we currently curate. (This overview was inspired by the [ORION-DBS initiative](https://orion-dbs.community/).)
+    **Inspiring initiatives** Below, you will see the ORI data resources we currently curate. This overview was inspired by the [ORION-DBS initiative](https://orion-dbs.community/), [Swemetrics](https://swemetrics.se/) and [Science Datatalake](https://j0nasw.github.io/science-datalake/) .
 
-    We want to add the following datasets: OpenAIRE, OpenALEX, OpenAPC, ROR, Harvest metadata from CRISes, Harvest metdata from Repositories, Crossref, SURF Journal Catalogue, CWTS Leiden Ranking, ORCID, DOAJ, DOAB, OpenCitations, DataCite, PKP beacon.
+    **Datasets wishlist**: OpenAIRE, OpenALEX, OpenAPC, ROR, Harvest metadata from CRISes, Harvest metdata from Repositories, Crossref, SURF Journal Catalogue, CWTS Leiden Ranking, ORCID, DOAJ, DOAB, OpenCitations, DataCite, PKP beacon.
     """)
-    mo.sidebar(
-        mo.accordion(
-            {"Background": background},
-            lazy=True,
-        )
-    )
+    mo.accordion({"Background": background},lazy=True,)
     return
 
 
-@app.cell
-def _(json, requests):
+@app.cell(hide_code=True)
+def config_file(json, requests):
+    # Get SURF object store configuration data
     response = requests.get('https://raw.githubusercontent.com/surf-ori/sprouts/refs/heads/notebooks/config.json')
     config = json.loads(response.text)
     return
 
 
-@app.cell
-def _(mo):
-    _df = mo.sql(
-        f"""
-        -- UPDATE __ducklake_metadata_sprouts.ducklake_metadata
-        -- SET value = 'https://objectstore.surf.nl/cea01a7216d64348b7e51e5f3fc1901d:sprouts-demo/data'
-        -- WHERE key = 'data_path'
-        """
-    )
-    return
-
-
-@app.cell
-def _(mo):
-    _df = mo.sql(
-        f"""
-        USE memory;
-        DETACH sprouts;
-        """
-    )
-    return
-
-
 @app.cell(hide_code=True)
-def _(mo):
+def catalog_section(mo):
     mo.md(r"""
     ## ORI data catalog
 
@@ -138,35 +112,19 @@ def _(mo):
     return
 
 
-@app.cell
-def _(mo):
+@app.cell(hide_code=True)
+def catalog_location(mo):
+    # show location of the ducklake catalog file
     url = mo.ui.text_area(value='https://objectstore.surf.nl/cea01a7216d64348b7e51e5f3fc1901d:sprouts/catalog.ducklake')
-    # url = mo.ui.text_area(value='https://objectstore.surf.nl/cea01a7216d64348b7e51e5f3fc1901d:boto3bucket/sprouts.ducklake')
     url
     return (url,)
 
 
-@app.cell(disabled=True)
-def _(url):
-    url.value
-    return
-
-
-@app.cell
-def _(mo):
-    _df = mo.sql(
-        f"""
-        USE memory;
-        DETACH sprouts;
-        """
-    )
-    return
-
-
-@app.cell
-def _(mo, url):
+@app.cell(hide_code=True)
+def catalog_loading(mo, url):
     sprouts_df = mo.sql(
         f"""
+        -- Loading catalog
         ATTACH 'ducklake:{url.value}' as sprouts;
         USE sprouts;
         """
@@ -174,10 +132,11 @@ def _(mo, url):
     return
 
 
-@app.cell
-def _(mo):
+@app.cell(hide_code=True)
+def catalog_statistics_data(mo):
     quick_statistics = mo.sql(
         f"""
+        -- Catalog quick statistics
         SELECT table_name, record_count, file_size_bytes
             FROM __ducklake_metadata_sprouts.ducklake_table_stats
             FULL JOIN __ducklake_metadata_sprouts.ducklake_table
@@ -188,8 +147,8 @@ def _(mo):
     return (quick_statistics,)
 
 
-@app.cell
-def _(datasets, mo, pl, quick_statistics, tables):
+@app.cell(hide_code=True)
+def catalog_sstatistics_cards(datasets, mo, pl, quick_statistics, tables):
     # Summary Statistics of ORI Datasets
 
     def _to_int(value):
@@ -231,15 +190,18 @@ def _(datasets, mo, pl, quick_statistics, tables):
 
 
 @app.cell(hide_code=True)
-def _():
-    ## ORI data resources
+def data_section(mo):
+    mo.md(r"""
+    ## ORI datasets, tables, columns
+    """)
     return
 
 
 @app.cell
-def _(mo):
+def data_datasets(mo):
     datasets = mo.sql(
         f"""
+        -- Get Data Sets
         FROM __ducklake_metadata_sprouts.ducklake_schema
         WHERE schema_name != 'main';
         """,
@@ -248,16 +210,22 @@ def _(mo):
     return (datasets,)
 
 
-@app.cell(hide_code=True)
-def _():
-    ## ORI tables
+@app.cell
+def _(mo):
+    _df = mo.sql(
+        f"""
+        SELECT * FROM "openaire"."datasets" LIMIT 100
+        """,
+        output=False
+    )
     return
 
 
 @app.cell
-def _(mo):
+def data_tables(mo):
     tables = mo.sql(
         f"""
+        -- Get Tables
         SELECT *
         FROM __ducklake_metadata_sprouts.ducklake_table t
         JOIN __ducklake_metadata_sprouts.ducklake_table_stats s
@@ -271,16 +239,11 @@ def _(mo):
     return (tables,)
 
 
-@app.cell(hide_code=True)
-def _():
-    ## ORI columns
-    return
-
-
 @app.cell
-def _(mo):
+def data_columns(mo):
     columns = mo.sql(
         f"""
+        -- Get columns
         SELECT *
         FROM __ducklake_metadata_sprouts.ducklake_column c
         JOIN __ducklake_metadata_sprouts.ducklake_column_tag t
@@ -289,14 +252,6 @@ def _(mo):
         output=False
     )
     return (columns,)
-
-
-@app.cell(hide_code=True)
-def _(mo):
-    mo.md(r"""
-    ## ORI datasets, tables, columns
-    """)
-    return
 
 
 @app.cell
@@ -382,6 +337,12 @@ def _(mo):
     editor = mo.ui.code_editor(value=initial_code, language="sql").form(submit_button_label="Run")
     editor
     return (editor,)
+
+
+@app.cell(hide_code=True)
+def _():
+    ## duckdb.sql("SELECT version()").fetchall()
+    return
 
 
 @app.cell(hide_code=True)
